2fb8ca174f6218cbb15ebf837461d11064aa5f72
Vadim Golub <git@vdmgolub.com>
2025-07-03 14:32:55 +0000

    chore: more debug

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@
 // and eslint is configured to lint ES6.
 
 module.exports = function(currentUser, matchingUsers, params, token) {
+  console.log(JSON.stringify({currentUser, matchingUsers, params, token}, null, 2));
   try {
     loadLinkPage(token);
   } catch (e) {
